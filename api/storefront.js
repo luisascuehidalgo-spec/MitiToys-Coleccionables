abf8e535f0f5a22177b410b65c5a423075e6d967
luisascuehidalgo-spec <luis.ascuehidalgo@gmail.com>
2026-09-03 08:29:03 -0300

    Load cart on storefront

--- api/storefront.js
+++ api/storefront.js
@@ -4,8 +4,8 @@ module.exports=async(req,res)=>{
   if(req.method!=='GET')return res.status(405).end();
   try{
     let html=fs.readFileSync(path.join(process.cwd(),'index.html'),'utf8');
-    const script='<script src="/catalogo-dinamico.js?v=1"></script>';
-    if(!html.includes('/catalogo-dinamico.js'))html=html.replace('</body>',script+'</body>');
+    const scripts='<script src="/carrito.js?v=1"></script><script src="/catalogo-dinamico.js?v=2"></script>';
+    if(!html.includes('/carrito.js'))html=html.replace('</body>',scripts+'</body>');
     res.setHeader('Content-Type','text/html; charset=utf-8');
     res.setHeader('Cache-Control','public, max-age=0, s-maxage=60, stale-while-revalidate=300');
     return res.status(200).send(html);
