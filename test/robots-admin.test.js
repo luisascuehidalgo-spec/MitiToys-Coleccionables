952d8d6a45fdfd38edef746f768f651d098b700e
luisascuehidalgo-spec <luis.ascuehidalgo@gmail.com>
2026-09-25 14:57:58 -0300

    Remove duplicate admin noindex assertion

--- test/robots-admin.test.js
+++ test/robots-admin.test.js
@@ -13,12 +13,3 @@ test('robots keeps admin surfaces and APIs out of crawler paths', () => {
 test('robots still advertises the canonical sitemap', () => {
   assert.match(robots, /^Sitemap: https:\/\/mititoys\.com\/sitemap\.xml$/m);
 });
-
-const admin = fs.readFileSync(require.resolve('../admin.html'), 'utf8');
-const adminEnvios = fs.readFileSync(require.resolve('../admin-envios.html'), 'utf8');
-
-test('admin HTML surfaces declare defense-in-depth noindex metadata', () => {
-  const noindex = /<meta\s+name=["']robots["']\s+content=["']noindex,nofollow,noarchive["']\s*\/?>/i;
-  assert.match(admin, noindex);
-  assert.match(adminEnvios, noindex);
-});
